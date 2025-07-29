c2f2426db137c39d565372bb8880016c9ca15132
MingyooLee <73154551+MingyooLee@users.noreply.github.com>
2025-07-29 09:18:51 +0900

    Test: CI/CD 테스트

--- src/test/java/com/coubee/coubeebeproduct/CoubeeBeProductApplicationTests.java
+++ src/test/java/com/coubee/coubeebeproduct/CoubeeBeProductApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CoubeeBeProductApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
