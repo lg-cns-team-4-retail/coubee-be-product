a44347c82dcabe5852b955294ba118e19b0ed233
SoonWookHwang <108779266+SoonWookHwang@users.noreply.github.com>
2025-08-18 14:18:11 +0900

    Fix: feign 역직렬화를 위한 ApiResponseDto에 기본생성자 추가

--- src/main/java/com/coubee/coubeebeproduct/common/dto/ApiResponseDto.java
+++ src/main/java/com/coubee/coubeebeproduct/common/dto/ApiResponseDto.java
@@ -1,8 +1,10 @@
 package com.coubee.coubeebeproduct.common.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ApiResponseDto<T> {
     private String code;
     private String message;
